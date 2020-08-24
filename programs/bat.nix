49f4e2ae216c9b788caa1383a7499f9872b81fa8
Shaw Vrana <shaw@vranix.com>
2020-08-24 14:29:17 -0700

    Failed attempt at installing the bat solarized theme

--- programs/bat.nix
+++ programs/bat.nix
@@ -4,8 +4,19 @@
   programs.bat = {
     enable = true;
     config = {
-      theme = "Solarized (dark)";
+      theme = "Monokai Extended Origin";
+      #theme = "solarized";
       style = "changes";
+    };
+
+    # hmm, can't set it with solarized above?
+    themes = {
+          solarized = builtins.readFile (pkgs.fetchFromGitHub {
+            owner = "braver";
+            repo = "Solarized"; # Bat uses sublime syntax for its themes
+            rev = "87e01090cf5fb821a234265b3138426ae84900e7";
+            sha256 = "01q2hn7rwccjcpgxl3xl7qrfrryhajmlkfv3mci6fbdgxnpvrg5w";
+          } + "/Solarized (dark).tmTheme");
     };
   };
 }
