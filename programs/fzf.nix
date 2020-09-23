673c8a75b69e28f557faa700dc2356a643f256cc
Shaw Vrana <shaw@vranix.com>
2020-09-22 20:11:59 -0700

    fzf: remove defaults from bound commands
    And preview dots

--- programs/fzf.nix
+++ programs/fzf.nix
@@ -5,6 +5,7 @@
     enable = true;
     defaultCommand = "fd --type f";
     defaultOptions = [
+      "--multi"
       "--reverse"
       "--info inline"
       "--bind=ctrl-f:page-down,ctrl-b:page-up,ctrl-d:page-up"
@@ -14,12 +15,12 @@
 
   programs.bash.initExtra = ''
     # open a file under the current directory
-    bind -x '"\C-p": f() { local file=$(fzf -m --height 80% --reverse) && [[ -n $file ]] && $EDITOR $file ; }; f'
+    bind -x '"\C-p": f() { local file=$(fzf -m --height 80%) && [[ -n $file ]] && $EDITOR $file ; }; f'
 
     # open a dotfile
-    bind -x '"\C-n": f() { pushd $DOTFILES > /dev/null ; local file=$(fzf -m --height 80% --reverse) && [[ -n $file ]] && $EDITOR $file ; popd > /dev/null ; }; f'
+    bind -x '"\C-n": f() { pushd $DOTFILES > /dev/null ; local file=$(fzf -m --height 80% --preview "bat --style=numbers --color=always --line-range :100 {}") && [[ -n $file ]] && $EDITOR $file ; popd > /dev/null ; }; f'
 
     # open a file under the current (git) project root
-    bind -x '"\C-f": f() { pushd $(git_root) > /dev/null ; local file=$(fzf -m --height 80% --reverse) && [[ -n $file ]] && $EDITOR $file ; popd > /dev/null ; }; f'
+    bind -x '"\C-f": f() { pushd $(git_root) > /dev/null ; local file=$(fzf -m --height 80%) && [[ -n $file ]] && $EDITOR $file ; popd > /dev/null ; }; f'
   '';
 }
