277c69e23d19cd391d31d433aaf827222eb90476
Shaw Vrana <shaw@vranix.com>
2020-11-07 15:24:00 -0800

    polybar launcher: fix match failure

--- scripts/polybar-launch.sh
+++ scripts/polybar-launch.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-killall -q polybar
-
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+killall -rq polybar
 
 # Launch polybar
 polybar top &
