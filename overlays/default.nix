2a3bb753fe9c05efe2c0f725e4a4d168f168d0d2
Shaw Vrana <shaw@vranix.com>
2020-12-27 15:44:47 -0800

    Move out overlays/default to flake/ branch
    It blocks home-manager switch from working

--- overlays/default.nix
+++
@@ -1,7 +0,0 @@
-self: super: rec {
-  system-san-francisco-font = super.callPackage ../packages/system-san-francisco-font { };
-  san-francisco-mono-font = super.callPackage ../packages/san-francisco-mono-font { };
-  ctlptl = super.callPackage ../packages/ctlptl { };
-  neovim = super.callPackage ./neovim.nix { };
-  powerline-go = super.callPackage ./powerline-go.nix { };
-}
