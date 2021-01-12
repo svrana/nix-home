96fbd0ed7849a26ef375400c774201959efc4f4f
Shaw Vrana <shaw@vranix.com>
2021-01-11 17:49:36 -0800

    Remove brlaser driver. Use bonjour

--- roles/graphical/printing.nix
+++ roles/graphical/printing.nix
@@ -3,11 +3,13 @@
 {
   services.printing = {
     enable = true;
-    drivers = with pkgs; [ brlaser ];
   };
   # for printer discovery
   services.avahi.enable = true;
   # Important to resolve .local domains of printers, otherwise you get an error
   # like  "Impossible to connect to XXX.local: Name or service not known"
   services.avahi.nssmdns = true;
+  # bonjour (dnssd) has worked well for me, i.e.,
+  # lpinfo --include-schemes dnssd -v
+  # lpadmin -p printername -v dnssd://foobarbaz
 }
