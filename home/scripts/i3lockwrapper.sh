e0b6527fff2926420ecc27ebff86a1e9c0dea303
Shaw Vrana <shaw@vranix.com>
2021-01-12 15:35:20 -0800

    Toggle screenblanking before and after lock screen

--- home/scripts/i3lockwrapper.sh
+++ home/scripts/i3lockwrapper.sh
@@ -10,20 +10,25 @@ i3lock_running() {
     return 1
 }
 
+screenblank_enable() {
+    xset +dpms
+    xset s on
+}
+
+screenblank_disable() {
+    xset -dpms
+    xset s off
+}
 
 lock() {
     # save current workspace
     current=$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true).name' | cut -d"\"" -f2)
-
-    # go to empty workspace
+    # go to empty workspace in case I end up typing in a password and its not locked, so it doesn't end up in slack or something silly
     i3-msg workspace 7
 
-    # startup lock, blocking until unlocked
-    #i3-msg workspace 7 && i3-msg workspace 7 && i3lock -l -c 002b36 --ignore-empty-password --no-unlock-indicator --clock --nofork --datestr="%A, %B %d %G" --timesize=48 --datesize=24
-    #i3lock -c 002b36 --ignore-empty-password --no-unlock-indicator --clock --nofork --datestr="%A, %B %d %G" --timesize=48 --datesize=24
+    screenblank_enable
     i3lock-color -c 002b36 --ignore-empty-password --no-unlock-indicator --clock --nofork --datestr="%A, %B %d %G" --timesize=48 --datesize=24
-
-    #echo "$PATH" > /home/shaw/switchto
+    screenblank_disable
 
     # go to previous workspace
     i3-msg workspace "$current"
