35a10700df6a31b6516590e7d144e4d548aabe33
Shaw Vrana <shaw@vranix.com>
2021-02-08 12:25:56 -0800

    weechat more squashme

--- home/weechat.nix
+++ home/weechat.nix
@@ -7,4 +7,7 @@
   programs.bash.sessionVariables = {
     WEECHAT_HOME = "$XDG_CONFIG_HOME/weechat";
   };
+
+  xdg.configFile."weechat/weechat.conf".source = ./config/weechat.conf;
+  xdg.configFile."weechat/irc.conf".source = ../personal/irc/irc.conf;
 }
