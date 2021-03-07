8061ba21be02c18b0235245b4f3c7fe7a8c5cda9
Shaw Vrana <shaw@vranix.com>
2021-03-06 16:10:42 -0800

    X: failed attempte at moving .XAuthority out of ~/
    AppImages don't have /run mapped in so they cannot access
    xauthority if there, so this didn't work as I use an appimage.

--- roles/graphical/x.nix
+++ roles/graphical/x.nix
@@ -5,16 +5,21 @@
     xkbOptions = "ctrl:nocaps";
     layout = "us";
     displayManager = {
-      defaultSession = "none+i3";
+      defaultSession = "home-manager";
+      job.logToFile = false;
       lightdm = {
         enable = true;
         greeters.gtk = {
           enable = true;
         };
+        # move .Xauthority out of home directory. Unfortunately appimages
+        # don't seem be able able to get to the Xauthority file.
+        #extraConfig = "user-authority-in-system-dir = true";
       };
     };
     desktopManager.session = [
       {
+        # mostly so we don't have to have ~/.xsession file
         name = "home-manager";
         start = ''
           ${pkgs.runtimeShell} $HOME/.config/X11/xsession &
