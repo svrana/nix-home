a305e34da932ec64d14bad87469bd59b1fa28786
Shaw Vrana <shaw@vranix.com>
2021-03-07 10:32:13 -0800

    pulse: failed attempt to move out esd_auth from ~/

--- roles/graphical/sound.nix
+++ roles/graphical/sound.nix
@@ -11,7 +11,16 @@
     # NixOS allows either a lightweight build (default) or full build of PulseAudio to be installed.
     # Only the full build has Bluetooth support, so it must be selected here.
     package = pkgs.pulseaudioFull;
-    extraConfig = "load-module module-switch-on-connect";
+
+    extraConfig = ''
+      load-module module-switch-on-connect
+    '';
+    # module-esound-protocol-tcp
+    # attempt to move this modules ~/esd-auth out of the ~/
+    # no work
+    # extraClientConf = ''
+    #   auth-cookie = .config/pulse/esd-auth-cookie
+    # '';
   };
 
   environment.systemPackages = with pkgs; [
