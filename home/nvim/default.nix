6b99f1dd5a8d6be65ee1dfefd92b06693fcb3db9
Shaw Vrana <shaw@vranix.com>
2021-08-19 17:34:03 -0700

    neovim migration to builtin lsp
    Comment out coc for now.
    
    goto definition, signatures while typing, autocode formatting
    
    Some things still going through neoformat/neomake but fine for now.

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -18,7 +18,7 @@ in
     withPython3 = true;
     withNodeJs = true;
     coc = {
-      enable = true;
+      enable = false;
       settings = {
         languageserver = {
           golang = {
@@ -37,6 +37,12 @@ in
     extraPackages = [
       pkgs.shfmt
       pkgs.nixfmt
+      pkgs.gopls
+      pkgs.nodePackages.bash-language-server
+      pkgs.nodePackages.typescript-language-server
+      pkgs.nodePackages.yaml-language-server
+      pkgs.nodePackages.json-server
+      pkgs.nodePackages.dockerfile-language-server-nodejs
     ];
     extraConfig = ''
         source $DOTFILES/home/nvim/init.vim
@@ -51,6 +57,179 @@ in
        {
         plugin = glow-nvim;
         config = "nnoremap <silent><LocalLeader>mg :Glow<CR>";
+       }
+       lsp_signature-nvim
+       {
+         plugin = nvim-compe;
+         config = ''
+          lua << EOF
+          require'compe'.setup {
+            enabled = true;
+            autocomplete = true;
+            debug = false;
+            min_length = 1;
+            preselect = 'enable';
+            throttle_time = 80;
+            source_timeout = 200;
+            resolve_timeout = 800;
+            incomplete_delay = 400;
+            max_abbr_width = 100;
+            max_kind_width = 100;
+            max_menu_width = 100;
+            documentation = {
+              border = { ''', ''' ,''', ' ', ''', ''', ''', ' ' }, -- the border option is the same as `|help nvim_open_win|`
+              winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
+              max_width = 120,
+              min_width = 60,
+              max_height = math.floor(vim.o.lines * 0.3),
+              min_height = 1,
+            };
+            source = {
+              path = true;
+              buffer = true;
+              nvim_lsp = true;
+              nvim_lua = true;
+            };
+          }
+
+          local t = function(str)
+            return vim.api.nvim_replace_termcodes(str, true, true, true)
+          end
+
+          local check_back_space = function()
+              local col = vim.fn.col('.') - 1
+              if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+                  return true
+              else
+                  return false
+              end
+          end
+
+          -- Use (s-)tab to:
+          --- move to prev/next item in completion menuone
+          --- jump to prev/next snippet's placeholder
+          _G.tab_complete = function()
+            if vim.fn.pumvisible() == 1 then
+              return t "<C-n>"
+            elseif check_back_space() then
+              return t "<Tab>"
+            else
+              return vim.fn['compe#complete']()
+            end
+          end
+          _G.s_tab_complete = function()
+            if vim.fn.pumvisible() == 1 then
+              return t "<C-p>"
+            else
+              return t "<S-Tab>"
+            end
+          end
+
+          vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+          vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+          vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+          vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+
+          --This line is important for auto-import
+          vim.api.nvim_set_keymap('i', '<cr>', 'compe#confirm("<cr>")', { expr = true })
+          vim.api.nvim_set_keymap('i', '<c-space>', 'compe#complete()', { expr = true })
+          EOF
+
+          lua << EOF
+          -- show the source of the diagnostic, useful if you have more than
+          -- one lsp server for the filetype, i.e., a linter as well as a compiler
+          -- (typescript)
+          vim.lsp.handlers["textDocument/publishDiagnostics"] =
+            function(_, _, params, client_id, _)
+              local config = { -- your config
+                underline = true,
+                virtual_text = {
+                  prefix = "■ ",
+                  spacing = 4,
+                },
+                signs = true,
+                update_in_insert = false,
+              }
+              local uri = params.uri
+              local bufnr = vim.uri_to_bufnr(uri)
+
+              if not bufnr then
+                return
+              end
+
+              local diagnostics = params.diagnostics
+
+              for i, v in ipairs(diagnostics) do
+                diagnostics[i].message = string.format("%s: %s", v.source, v.message)
+              end
+
+              vim.lsp.diagnostic.save(diagnostics, bufnr, client_id)
+
+              if not vim.api.nvim_buf_is_loaded(bufnr) then
+                return
+              end
+
+              vim.lsp.diagnostic.display(diagnostics, bufnr, client_id, config)
+          end
+          EOF
+         '';
+       }
+       {
+       plugin = nvim-lspconfig;
+       config = ''
+        lua << EOF
+        local nvim_lsp = require('lspconfig')
+
+        -- Use an on_attach function to only map the following keys
+        -- after the language server attaches to the current buffer
+        local on_attach = function(client, bufnr)
+          local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+          local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+          --Enable completion triggered by <c-x><c-o>
+          buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+          -- Mappings.
+          local opts = { noremap=true, silent=true }
+
+          -- See `:help vim.lsp.*` for documentation on any of the below functions
+          buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+          buf_set_keymap('n', '<C-]>', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+          buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+          buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+          buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+          buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+          buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+          buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+          buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+          buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+          buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+          buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+          buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+          buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+          buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+          buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+          buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+
+          require "lsp_signature".on_attach({
+            bind = true,
+            toggle_key='<C-x>'
+          })
+        end
+
+        -- Use a loop to conveniently call 'setup' on multiple servers and
+        -- map buffer local keybindings when the language server attaches
+        local servers = { 'gopls', 'tsserver', 'bashls', 'yamlls', 'rnix', 'dockerls'}
+        for _, lsp in ipairs(servers) do
+          nvim_lsp[lsp].setup {
+            on_attach = on_attach,
+            flags = {
+              debounce_text_changes = 150,
+            }
+          }
+        end
+        EOF
+       '';
        }
        {
          plugin = lualine-nvim;
@@ -114,64 +293,64 @@ in
        #vim-commentary
        vim-sort-motion
        vim-sneak
-       {
-         plugin = supertab;
-         config = ''
-            let g:SuperTabCrMapping = 0
-            let g:SuperTabDefaultCompletionType = '<c-n>'
-            let g:SuperTabClosePreviewOnPopupClose = 1
-         '';
-       }
-       {
-        plugin = coc-nvim;
-        config = ''
-          autocmd CursorHold * silent call CocActionAsync('highlight')
-
-          function! s:show_documentation()
-            if (index(['vim','help'], &filetype) >= 0)
-              execute 'h '.expand('<cword>')
-            elseif (coc#rpc#ready())
-              call CocActionAsync('doHover')
-            else
-              execute '!' . &keywordprg . " " . expand('<cword>')
-            endif
-          endfunction
-
-          nnoremap <silent> K :call <SID>show_documentation()<CR>
-          " Show signature help on placeholder jump
-          autocmd User CocJumpPlaceholder call CocActionAsync('showSignatureHelp')
-
-          nmap <silent> <leader>cd <Plug>(coc-definition)
-          nmap <silent> <leader>cc <Plug>(coc-references)
-          nmap <silent> <leader>ci <Plug>(coc-implementation)
-          nmap <silent> <leader>cp <Plug>(coc-diagnostic-prev)
-          nmap <silent> <leader>cn <Plug>(coc-diagnostic-next
-          nmap <silent> <leader>ct <Plug>(coc-type-definition)
-          nmap <silent> <leader>cr <Plug>(coc-rename)
-          nmap <silent> <leader>cl <esc>(coc-list-location)<CR><CR>
-          nmap <silent> <leader>cs <esc>:CocRestart<CR><CR>
-        '';
-       }
-       coc-json
-       coc-yaml
-       coc-python
-       coc-git
-       coc-tsserver
-       coc-tslint-plugin
-       {
-          plugin = coc-snippets;
-          config = ''
-            vmap <C-j> <Plug>(coc-snippets-select)
-            " Use <C-j> for jump to next placeholder, it's default of coc.nvim
-            let g:coc_snippet_next = '<c-j>'
-            " Use <C-k> for jump to previous placeholder, it's default of coc.nvim
-            let g:coc_snippet_prev = '<c-k>'
-            " Use <C-j> for both expand and jump (make expand higher priority.)
-            imap <C-j> <Plug>(coc-snippets-expand-jump)
-            " Use <leader>sn for convert visual selected code to snippet (i.e., snippet new)
-            xmap <leader>sn  <Plug>(coc-convert-snippet)
-          '';
-       }
+       # {
+       #   plugin = supertab;
+       #   config = ''
+       #      let g:SuperTabCrMapping = 0
+       #      let g:SuperTabDefaultCompletionType = '<c-n>'
+       #      let g:SuperTabClosePreviewOnPopupClose = 1
+       #   '';
+       # }
+       # {
+       #  plugin = coc-nvim;
+       #  config = ''
+       #    autocmd CursorHold * silent call CocActionAsync('highlight')
+       #
+       #    function! s:show_documentation()
+       #      if (index(['vim','help'], &filetype) >= 0)
+       #        execute 'h '.expand('<cword>')
+       #      elseif (coc#rpc#ready())
+       #        call CocActionAsync('doHover')
+       #      else
+       #        execute '!' . &keywordprg . " " . expand('<cword>')
+       #      endif
+       #    endfunction
+       #
+       #    nnoremap <silent> K :call <SID>show_documentation()<CR>
+       #    " Show signature help on placeholder jump
+       #    autocmd User CocJumpPlaceholder call CocActionAsync('showSignatureHelp')
+       #
+       #    nmap <silent> <leader>cd <Plug>(coc-definition)
+       #    nmap <silent> <leader>cc <Plug>(coc-references)
+       #    nmap <silent> <leader>ci <Plug>(coc-implementation)
+       #    nmap <silent> <leader>cp <Plug>(coc-diagnostic-prev)
+       #    nmap <silent> <leader>cn <Plug>(coc-diagnostic-next
+       #    nmap <silent> <leader>ct <Plug>(coc-type-definition)
+       #    nmap <silent> <leader>cr <Plug>(coc-rename)
+       #    nmap <silent> <leader>cl <esc>(coc-list-location)<CR><CR>
+       #    nmap <silent> <leader>cs <esc>:CocRestart<CR><CR>
+       #  '';
+       # }
+       # coc-json
+       # coc-yaml
+       # coc-python
+       # coc-git
+       # coc-tsserver
+       # coc-tslint-plugin
+       # {
+       #    plugin = coc-snippets;
+       #    config = ''
+       #      vmap <C-j> <Plug>(coc-snippets-select)
+       #      " Use <C-j> for jump to next placeholder, it's default of coc.nvim
+       #      let g:coc_snippet_next = '<c-j>'
+       #      " Use <C-k> for jump to previous placeholder, it's default of coc.nvim
+       #      let g:coc_snippet_prev = '<c-k>'
+       #      " Use <C-j> for both expand and jump (make expand higher priority.)
+       #      imap <C-j> <Plug>(coc-snippets-expand-jump)
+       #      " Use <leader>sn for convert visual selected code to snippet (i.e., snippet new)
+       #      xmap <leader>sn  <Plug>(coc-convert-snippet)
+       #    '';
+       # }
        #coc-protobuf
        {
          plugin = neoformat;
@@ -205,7 +384,7 @@ in
        }
        tmux-complete-vim
        vim-snippets
-       vim-autoformat
+       #vim-autoformat
        {
          plugin = vim-rooter;
          config = "let g:rooter_cd_cmd = 'lcd'";
@@ -217,9 +396,12 @@ in
            let g:go_fmt_autosave = 1
            let g:go_metalinter_autosave_enabled = ['gopls', 'vet']
            let g:go_list_type = "quickfix"
-           let g:go_def_mode='gopls'
            let g:go_info_mode='gopls'
-           " disable mapping of K to godoc. We remap for coc for the same purpose but prettier
+           " let lsp handle ctrl-]
+           let g:go_def_mapping_enabled=0
+           " pin our versions with nix
+           let g:go_get_update=0
+           " disable mapping of K to godoc
            let g:go_doc_keywordprg_enabled = 0
 
            " run :GoBuild or :GoTestCompile based on the go file
@@ -292,25 +474,6 @@ in
            nnoremap <silent><leader>ft :NvimTreeToggle<CR>
          '';
        }
-      # {
-      #    plugin = vim-nerdtree-tabs;
-      #    config = ''
-      #       map <silent> <leader>ft <plug>NERDTreeTabsToggle<CR>
-      #    '';
-      #  }
-      #  {
-      #    plugin = nerdtree-git-plugin;
-      #    config = ''
-      #      let g:NERDTreeGitStatusConcealBrackets = 1
-      #    '';
-      #  }
-      #  {
-      #   plugin = nerdtree;
-      #   config = ''
-      #     let NERDTreeMinimalUI = 1
-      #     let NERDTreeStatusline=""
-      #   '';
-      # }
     ];
   };
 
--- home/nvim/init.vim
+++ home/nvim/init.vim
@@ -22,7 +22,7 @@ set noequalalways
 set updatetime=100
 set lazyredraw
 set rtp+=$RCS/nvim/vimsnips
-set tagfunc=CocTagFunc
+"set tagfunc=CocTagFunc
 set background=dark
 set pyx=3
 set termguicolors
@@ -37,7 +37,7 @@ highlight WhiteOnRed ctermfg=white ctermbg=red
 
 autocmd! BufWritePre * :%s/\s\+$//e
 
-autocmd CursorHold * silent call CocActionAsync('highlight')
+"autocmd CursorHold * silent call CocActionAsync('highlight')
 autocmd FileType * setlocal formatoptions+=croq
 autocmd BufRead gitcommit setlocal spell spelllang=en_US textwidth=72
 autocmd BufRead gitcommit setlocal fo+=t
@@ -189,3 +189,74 @@ function MyTabLabel(n)
 endfunction
 
 set tabline=%!MyTabLine()
+
+lua << EOF
+local lsp = require 'vim.lsp'
+local util = require 'vim.lsp.util'
+local log = require 'vim.lsp.log'
+local vim = vim
+
+-- Ref (in Japanese): https://daisuzu.hatenablog.com/entry/2019/12/06/005543
+-- Ref: https://qrunch.net/@igrep/entries/K6sUDofcmvtnRqzk
+function tagfunc_nvim_lsp(pattern, flags, info)
+ local result = {}
+ local isSearchingFromNormalMode = flags == "c"
+
+ local method
+ local params
+ if isSearchingFromNormalMode then
+   -- Jump to the definition of the symbol under the cursor
+   -- when called by CTRL-]
+   method = 'textDocument/definition'
+   params = util.make_position_params()
+ else
+   -- NOTE: Currently I'm not sure how this clause is tested
+   --       because `:tag` command doesn't seem to use `tagfunc`.
+
+   -- Search with `pattern` when called by ex command (e.g. `:tag`)
+   method = 'workspace/symbol'
+
+   -- Delete "\<" from `pattern` when prepended.
+   -- Perhaps the server doesn't support regex in vim!
+   params = {}
+   if string.find(flags, 'i') then
+     params.query = string.sub(pattern, '^\\<', '')
+   else
+     params.query = pattern
+   end
+ end
+ local client_id_to_results, err = lsp.buf_request_sync(0, method, params, 800)
+ if err then
+   print('Error when calling tagfunc: ' .. err)
+   return result
+ end
+
+ for _client_id, results in pairs(client_id_to_results) do
+   for i, lsp_result in ipairs(results.result) do
+     local name
+     local location
+     if isSearchingFromNormalMode then
+       name = pattern
+       location = lsp_result
+     else
+       name = lsp_result.name
+       location = lsp_result.location
+     end
+     local location_for_tagfunc = {
+       name = name,
+       filename = vim.uri_to_fname(location.uri),
+       cmd = tostring(location.range.start.line + 1)
+     }
+     table.insert(result, location_for_tagfunc)
+ end
+ end
+ return result
+end
+EOF
+setlocal tagfunc=v:lua.tagfunc_nvim_lsp
+" nvim-compe
+set completeopt=menuone,noselect
+
+"have neoformat handle typescript formatting
+"autocmd BufWritePre *.ts lua vim.lsp.buf.formatting_sync(nil, 5000)
+"autocmd BufWritePre *.tsx lua vim.lsp.buf.formatting_sync(nil, 5000)
