8e0bd630385f7099c9ce1ab77ab20c11a01004e1
Shaw Vrana <shaw@vranix.com>
2021-09-20 20:46:05 -0700

    nvim-compe -> nvim-cmp

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -222,80 +222,57 @@ in
         '';
       }
       lsp_signature-nvim
+      luasnip
+      cmp_luasnip
+      #cmp_path
+      #cmp_buffer
+      cmp-nvim-lsp
       {
-        plugin = nvim-compe;
+        plugin = nvim-cmp;
         config = lua ''
-          require('compe').setup {
-            enabled = true;
-            autocomplete = true;
-            debug = false;
-            min_length = 1;
-            preselect = 'enable';
-            throttle_time = 80;
-            source_timeout = 200;
-            resolve_timeout = 800;
-            incomplete_delay = 400;
-            max_abbr_width = 100;
-            max_kind_width = 100;
-            max_menu_width = 100;
-            documentation = {
-              border = { ''', ''' ,''', ' ', ''', ''', ''', ' ' }, -- the border option is the same as `|help nvim_open_win|`
-              winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
-              --max_width = 120,
-              --min_width = 60,
-              --max_height = math.floor(vim.o.lines * 0.3),
-              --min_height = 1,
-            };
-            source = {
-              path = true;
-              buffer = true;
-              nvim_lsp = true;
-              nvim_lua = true;
-              tmux = true;
-            };
-          }
-
-          local t = function(str)
-            return vim.api.nvim_replace_termcodes(str, true, true, true)
-          end
-
-          local check_back_space = function()
-              local col = vim.fn.col('.') - 1
-              if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-                  return true
-              else
-                  return false
-              end
-          end
-
-          -- Use (s-)tab to:
-          --- move to prev/next item in completion menuone
-          --- jump to prev/next snippet's placeholder
-          _G.tab_complete = function()
-            if vim.fn.pumvisible() == 1 then
-              return t "<C-n>"
-            elseif check_back_space() then
-              return t "<Tab>"
-            else
-              return vim.fn['compe#complete']()
-            end
-          end
-          _G.s_tab_complete = function()
-            if vim.fn.pumvisible() == 1 then
-              return t "<C-p>"
-            else
-              return t "<S-Tab>"
-            end
-          end
-
-          vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
-          vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
-          vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-          vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-
-          --This line is important for auto-import
-          vim.api.nvim_set_keymap('i', '<cr>', 'compe#confirm("<cr>")', { expr = true })
-          vim.api.nvim_set_keymap('i', '<c-space>', 'compe#complete()', { expr = true })
+           local luasnip = require 'luasnip'
+           local cmp = require 'cmp'
+           cmp.setup {
+             snippet = {
+               expand = function(args)
+                 require('luasnip').lsp_expand(args.body)
+               end,
+             },
+             mapping = {
+               ['<C-p>'] = cmp.mapping.select_prev_item(),
+               ['<C-n>'] = cmp.mapping.select_next_item(),
+               ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+               ['<C-f>'] = cmp.mapping.scroll_docs(4),
+               ['<C-Space>'] = cmp.mapping.complete(),
+               ['<C-e>'] = cmp.mapping.close(),
+               ['<CR>'] = cmp.mapping.confirm {
+                 behavior = cmp.ConfirmBehavior.Replace,
+                 select = true,
+               },
+               ['<Tab>'] = function(fallback)
+                 if vim.fn.pumvisible() == 1 then
+                   vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true, true, true), 'n')
+                 elseif luasnip.expand_or_jumpable() then
+                   vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), "")
+                 else
+                   fallback()
+                 end
+               end,
+               ['<S-Tab>'] = function(fallback)
+                 if vim.fn.pumvisible() == 1 then
+                   vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-p>', true, true, true), 'n')
+                 elseif luasnip.jumpable(-1) then
+                   vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), "")
+                 else
+                   fallback()
+                 end
+               end,
+             },
+             sources = {
+               { name = 'nvim_lsp' },
+               { name = 'luasnip' },
+             },
+           }
 
           -- show the source of the diagnostic, useful if you have more than
           -- one lsp server for the filetype, i.e., a linter as well as a compiler
@@ -401,6 +378,7 @@ in
           for _, lsp in ipairs(servers) do
             nvim_lsp[lsp].setup({
               on_attach = on_attach,
+              capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
               flags = {
                 debounce_text_changes = 150,
               }
@@ -419,6 +397,7 @@ in
                 client.resolved_capabilities.document_range_formatting = false
                 on_attach(client)
             end,
+            capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
           }
 
           local sumneko_root_path = "${pkgs.sumneko-lua-language-server}/extas"
@@ -429,6 +408,7 @@ in
 
           require('lspconfig').sumneko_lua.setup {
             on_attach = on_attach,
+            capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
             flags = {
               debounce_text_changes = 150,
             },
