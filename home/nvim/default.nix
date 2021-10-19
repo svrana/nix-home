7de9d5176f71b06d39f1212b1dcb2eebf74c8b3d
Shaw Vrana <shaw@vranix.com>
2021-10-19 15:38:09 -0700

    remove unused tabline plugins

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -116,29 +116,8 @@ in
       }
       glow-nvim
       goimpl-nvim
-      # {
-      #   plugin = luatab-nvim;
-      #   config = lua ''
-      #     vim.o.tabline = '%!v:lua.require\'luatab\'.tabline()'
-      #   '';
-      # }
       lualine-lsp-progress
       nvim-web-devicons
-      # {
-      #   plugin = nvim-tabline;
-      #   config = lua ''
-      #     require('tabline').setup{
-      #         no_name = '[No Name]',    -- Name for buffers with no name
-      #         modified_icon = '',      -- Icon for showing modified buffer
-      #         close_icon = '',         -- Icon for closing tab with mouse
-      #         separator = "▌",          -- Separator icon on the left side
-      #         padding = 3,              -- Prefix and suffix space
-      #         color_all_icons = false,  -- Color devicons in active and inactive tabs
-      #         always_show_tabs = false, -- Always show tabline
-      #         right_separator = false,  -- Show right separator on the last tab
-      #     }
-      #   '';
-      # }
       plenary-nvim
       {
         plugin = null-ls-nvim;
