f5c79d401f3fe4c61541e3fa37606e928150fa86
Shaw Vrana <shaw@vranix.com>
2021-10-19 19:35:10 -0700

    lspkind setup go with the times

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -357,21 +357,17 @@ in
               { name = 'path' },
             },
             formatting = {
-              deprecated = true,
-              format = function(entry, vim_item)
-              -- fancy icons and a name of kind
-              vim_item.kind = require("lspkind").presets.default[vim_item.kind] .. " " .. vim_item.kind
-
-              -- set a name for each source
-              vim_item.menu = ({
-                buffer = "[Buffer]",
-                nvim_lsp = "[LSP]",
-                luasnip = "[LuaSnip]",
-                nvim_lua = "[Lua]",
-                latex_symbols = "[Latex]",
-              })[entry.source.name]
-              return vim_item
-              end,
+              format = require('lspkind').cmp_format(
+                {
+                  with_text = true,
+                  menu = ({
+                    buffer = "[Buffer]",
+                    nvim_lsp = "[LSP]",
+                    luasnip = "[LuaSnip]",
+                    nvim_lua = "[Lua]",
+                    latex_symbols = "[Latex]",
+                  })
+                }),
             }
           }
 
