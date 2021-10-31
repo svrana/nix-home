7ca80a3831436868d51bf4d9e50c3401fd3f1326
Shaw Vrana <shaw@vranix.com>
2021-10-31 16:15:21 -0700

    Change ownership of hidraw devices
    So I can access them

--- roles/standard/udev.nix
+++ roles/standard/udev.nix
@@ -2,6 +2,7 @@
   services.udev.extraRules = ''
     # Required for Ledger Live to detect Ledger Nano X via USB
     SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0004", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
+    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0664", GROUP="plugdev"
   ''
   ;
 }
