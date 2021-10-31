1c6ceff953a7e49603434b2fc5907008da0cc6a4
Shaw Vrana <shaw@vranix.com>
2021-10-31 16:15:12 -0700

    dunst change of configs upstream

--- home/dunst.nix
+++ home/dunst.nix
@@ -5,7 +5,12 @@
     enable = true;
     settings = {
       global = {
-        geometry = "500x50-30+35";
+        #geometry = "500x50-30+35";
+        #origin = "top,right";
+        width = "400";
+        height = "10000";
+        offset = "50x30";
+        #notification_height = 35;
         indicate_hidden = "yes";
         shrink = "no";
         separator_height = 4;
