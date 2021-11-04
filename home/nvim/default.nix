c1666a22e73f16e58d0f8bf6b1d060fb226300ea
Shaw Vrana <shaw@vranix.com>
2021-11-04 10:36:01 -0700

    nvim: use whichkey for buffer local maps/lsp

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -492,6 +492,7 @@ in
         config = lua ''
           local nvim_lsp = require('lspconfig')
           local protocol = require('vim.lsp.protocol')
+          local wk = require("which-key")
 
           local diagnostic_map = function(bufnr)
             local opts = {noremap = true, silent = true}
@@ -526,26 +527,19 @@ in
             local opts = { noremap=true, silent=true }
 
             -- See `:help vim.lsp.*` for documentation on any of the below functions
-            buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-            buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-            buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-            buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-            buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-            buf_set_keymap('n', '<C-]>', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-            buf_set_keymap('n', '<leader>cn', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-
-            --buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-            --buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-            --buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-            --buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-            --buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-            --buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-            --buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-            -- this is nice
-            --buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-            --buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-
-            require('lsp_signature').on_attach()
+            wk.register({
+              [ '<C-k>' ]   = { "<cmd>lua vim.lsp.buf.signature_help()<cr>",    "method signature",     buffer = bufnr },
+              [ '<C-]>' ]   = { "<cmd>lua vim.lsp.buf.definition()<cr>",        "goto definition",      buffer = bufnr },
+              [ "[d" ]      = { "<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>",  "prev diagnostic", buffer = bufnr },
+              [ "]d" ]      = { "<cmd>lua vim.lsp.diagnostic.goto_next()<cr>",  "next diagnostic", buffer = bufnr },
+              ["<leader>"]  = {
+                c = {
+                  n = { "<cmd>lua vim.lsp.buf.references()<cr>",      "show callers", buffer = bufnr },
+                  D = { "<cmd>lua vim.lsp.buf.type_definition()<cr>", "show type definition", buffer = bufnr },
+                },
+              },
+              ['gi'] = {  "<cmd>lua vim.lsp.buf.implementation()<cr>", "goto implementation", buffer = bufnr },
+            })
           end
 
           local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
@@ -787,7 +781,6 @@ in
               ["<leader>"] = {
                 c = {
                   name = "code",
-                  -- buf_set_keymap('n', '<leader>cr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
                   d = { "<cmd>Lspsaga show_line_diagnostics<cr>", "show line Diagnostics"},
                   a = { "<cmd>Lspsaga code_action<cr>", "run Action" },
                   f = { "<cmd>Lspsaga lsp_finder<cr>", "Find usage" },
