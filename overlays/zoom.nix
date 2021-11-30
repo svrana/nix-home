39dd316721a7329f5b720b3b300890b2d5f71370
Shaw Vrana <shaw@vranix.com>
2021-11-29 17:12:02 -0800

    mo swaylock tweak

--- overlays/zoom.nix
+++ overlays/zoom.nix
@@ -2,12 +2,12 @@ self: super:
 {
   zoom-us = super.zoom-us.overrideAttrs (old: {
     postFixup = old.postFixup + ''
-      wrapProgram $out/bin/zoom-us --unset XDG_SESSION_TYPE
+      wrapProgram $out/bin/zoom-us --unset XDG_SESSION_TYPE --unset WAYLAND_DISPLAY
     '';
   });
   zoom = super.zoom-us.overrideAttrs (old: {
     postFixup = old.postFixup + ''
-      wrapProgram $out/bin/zoom --unset XDG_SESSION_TYPE
+      wrapProgram $out/bin/zoom --unset XDG_SESSION_TYPE --unset WAYLAND_DISPLAY
     '';
   });
 }
