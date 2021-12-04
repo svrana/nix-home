9d7e216f92aefa5d20b25c129b325839bef03f32
Shaw Vrana <shaw@vranix.com>
2021-12-03 18:36:39 -0800

    use lbonn's wayland via override courtesy @berbiche

--- home/rofi.nix
+++ home/rofi.nix
@@ -2,7 +2,7 @@
 {
   programs.rofi = {
     enable = true;
-    package = pkgs.rofi.override { plugins = [ pkgs.rofi-calc pkgs.rofi-emoji ]; };
+    package = pkgs.rofi-wayland.override { plugins = [ pkgs.rofi-calc pkgs.rofi-emoji ]; };
     theme = "solarized";
     font = "SFNS ${toString config.settings.rofi.fontSize}";
     terminal = "${pkgs.alacritty}/bin/alacritty";
