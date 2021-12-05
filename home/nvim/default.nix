f45d1217e650d35f47b73340f995562103b7d321
Shaw Vrana <shaw@vranix.com>
2021-12-05 09:23:13 -0800

    nvim: comment alignment

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -208,19 +208,19 @@ in
         config = lua ''
            require('neorg').setup {
              load = {
-               ["core.defaults"] = {}, -- Load all the default modules
-               ["core.norg.concealer"] = {}, -- Allows for use of icons
-               ["core.norg.dirman"] = { -- Manage your directories with Neorg
+               ["core.defaults"] = {},              -- Load all the default modules
+               ["core.norg.concealer"] = {},        -- Allows for use of icons
+               ["core.norg.dirman"] = {             -- Manage your directories with Neorg
                ["core.norg.completion"] = {
                 config = {
                    engine = "nvim-cmp"
                  },
                },
                ["core.keybinds"] = {
-                config = {
-                        default_keybinds = true, -- Generate the default keybinds
-                        neorg_leader = "<Leader>o" -- This is the default if unspecified
-                },
+                 config = {
+                   default_keybinds = true,         -- Generate the default keybinds
+                   neorg_leader = "<Leader>o"       -- This is the default if unspecified
+                 },
                },
                config = {
                  workspaces = {
