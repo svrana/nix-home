24e0cb2937fadec90195ddfc50c179ea1fb049b7
Shaw Vrana <shaw@vranix.com>
2021-12-19 13:19:20 -0800

    nvim: use commentstring properly with nvim-comment

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -302,7 +302,8 @@ in
                 -- see nvim-ts-context-commentstring
                 context_commentstring = {
                   enable = true,
-                }
+                  enable_autocmd = false, -- turn on manually with comment plugin, see docs
+                },
               }
           '';
       }
@@ -714,27 +715,21 @@ in
         plugin = comment-nvim;
         config = lua ''
           require('Comment').setup({
+            -- delegate to nvim-ts-context-commentstring for figuring out commentstring type
             pre_hook = function(ctx)
-              -- Only calculate commentstring for tsx filetypes
-              if vim.bo.filetype == 'typescriptreact' then
-                local U = require('Comment.utils')
+              local U = require 'Comment.utils'
 
-                -- Detemine whether to use linewise or blockwise commentstring
-                local type = ctx.ctype == U.ctype.line and '__default' or '__multiline'
-
-                -- Determine the location where to calculate commentstring from
-                local location = nil
-                if ctx.ctype == U.ctype.block then
-                  location = require('ts_context_commentstring.utils').get_cursor_location()
-                elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-                  location = require('ts_context_commentstring.utils').get_visual_start_location()
-                end
-
-                return require('ts_context_commentstring.internal').calculate_commentstring({
-                  key = type,
-                  location = location,
-                })
+              local location = nil
+              if ctx.ctype == U.ctype.block then
+                location = require('ts_context_commentstring.utils').get_cursor_location()
+              elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+                location = require('ts_context_commentstring.utils').get_visual_start_location()
               end
+
+              return require('ts_context_commentstring.internal').calculate_commentstring {
+                key = ctx.ctype == U.ctype.line and '__default' or '__multiline',
+                location = location,
+              }
             end,
           })
         '';
