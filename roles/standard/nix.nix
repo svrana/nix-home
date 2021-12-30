6492b795f628fee937270ba00a1a514dbaac4afc
Shaw Vrana <shaw@vranix.com>
2021-12-30 08:41:41 -0800

    nix: enable nix-command
    Now needed by vim plugins update.py

--- roles/standard/nix.nix
+++ roles/standard/nix.nix
@@ -1,18 +1,17 @@
 { pkgs, ... }:
 {
   nix = {
-    #package = pkgs.nixUnstable;
     nixPath = import ../../nix-path.nix;
     trustedUsers = [ "@wheel" ];
     # Automatically optimize the Nix store to save space by hard-linking
     # identical files together. These savings add up.
-    autoOptimiseStore  = true;
+    autoOptimiseStore = true;
     # keep-* for nix-direnv nix-shell generations
     # hmm, getting a warning with this: warning: unknown setting 'experimental-features'
-    #  experimental-features = nix-command
     extraOptions = ''
       keep-outputs = true
       keep-derivations = true
+      experimental-features = nix-command
     '';
   };
 }
