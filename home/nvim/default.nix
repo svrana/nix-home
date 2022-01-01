654305d43b03ed2f0e9c5b1bc40425af273f398b
Shaw Vrana <shaw@vranix.com>
2022-01-01 14:43:44 -0800

    nvim: fix up lsp-progress plugin

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -650,6 +650,8 @@ in
                  'filename',
                  file_status = true, -- displays file status (readonly status, modified status)
                  path = 1,           -- 0 = just filename, 1 = relative path, 2 = absolute path
+               },
+               {
                  'lsp_progress'
                }
              },
