32c49d193bb577c0e6be804722a448ef04379d0c
Shaw Vrana <shaw@vranix.com>
2022-02-23 08:01:00 -0800

    Fix printing

--- roles/graphical/printing.nix
+++ roles/graphical/printing.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
   services.printing = {
@@ -9,8 +9,15 @@
   services.avahi.enable = true;
   # Important to resolve .local domains of printers, otherwise you get an error
   # like  "Impossible to connect to XXX.local: Name or service not known"
-  services.avahi.nssmdns = true;
+  #services.avahi.nssmdns = true;
   # bonjour (dnssd) has worked well for me, i.e.,
   # lpinfo --include-schemes dnssd -v
   # lpadmin -p printername -v dnssd://foobarbaz
+  services.avahi.nssmdns = false; # Use my settings from below
+  # settings from avahi-daemon.nix where mdns is replaced with mdns4
+  system.nssModules = with pkgs.lib; optional (!config.services.avahi.nssmdns) pkgs.nssmdns;
+  system.nssDatabases.hosts = with pkgs.lib; optionals (!config.services.avahi.nssmdns) (mkMerge [
+    (mkOrder 900 [ "mdns4_minimal [NOTFOUND=return]" ]) # must be before resolve
+    (mkOrder 1501 [ "mdns4" ]) # 1501 to ensure it's after dns
+  ]);
 }
