9628ca9087bb784642a1a3ca65bb5a7d60c3ec2a
Shaw Vrana <shaw@vranix.com>
2022-04-17 18:44:30 -0700

    ctptl bump

--- packages/ctlptl/default.nix
+++ packages/ctlptl/default.nix
@@ -2,19 +2,21 @@
 
 buildGoModule rec {
   pname = "ctlptl";
-  version = "0.6.0";
+  version = "0.7.8";
 
   src = fetchFromGitHub {
     owner = "tilt-dev";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0vass9vdrl90q809gf886hijk85653is4v5f249zzif8iy00haj9";
+    sha256 = "li+WwDmJ5aKmKVg8n/gWmJSBafDa4WLSNitIVBk0ZOY=";
   };
 
-  vendorSha256 = "0y43mnqshlc6x28v0z7wnb76irz9lf1sg27y332aq9z55sbsgwmc";
+  vendorSha256 = "JpCpyjH4XhPPzgUv/yICVR1p7jV7bOIVbKHe28i+Hgc=";
 
-  buildFlagsArray = [
-    "-ldflags=-s -w -X main.version=${version}"
+  ldFlags = [
+    "-s"
+    "-w"
+    "-X main.version=${version}"
   ];
 
   meta = with pkgs.lib; {
