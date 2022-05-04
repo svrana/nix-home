7c0a24b8e68f59937af3a30349f2064e6a74c0c4
Shaw Vrana <shaw@vranix.com>
2022-05-04 16:03:18 -0700

    cachix: add digitallyinduced for ihp

--- roles/standard/nix.nix
+++ roles/standard/nix.nix
@@ -8,12 +8,17 @@
         "https://nix-community.cachix.org"
         "https://hydra.iohk.io"
         "https://iohk.cachix.org"
+        "https://nrdxp.cachix.org"
+        "https://digitallyinduced.cachix.org"
+
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
         "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
+        "nrdxp.cachix.org-1:Fc5PSqY2Jm1TrWfm88l6cvGWwz3s93c6IOifQWnhNW4="
+        "digitallyinduced.cachix.org-1:y+wQvrnxQ+PdEsCt91rmvv39qRCYzEgGQaldK26hCKE="
       ];
       auto-optimise-store = true;
     };
