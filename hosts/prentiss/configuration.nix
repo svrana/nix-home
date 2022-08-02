96cff70d9847e16923d981f5f2385a8599360df9
Shaw Vrana <shaw@vranix.com>
2022-08-01 20:34:50 -0700

    add v4l2loopback module

--- hosts/prentiss/configuration.nix
+++ hosts/prentiss/configuration.nix
@@ -11,6 +11,19 @@
     ../../roles/work
   ];
 
+  boot.extraModulePackages = with config.boot.kernelPackages;
+    [ v4l2loopback.out ];
+  boot.kernelModules = [
+    "v4l2loopback"
+    "snd-aloop"
+  ];
+  boot.extraModprobeConfig = ''
+    # exclusive_caps: Skype, Zoom, Teams etc. will only show device when actually streaming
+    # card_label: Name of virtual camera, how it'll show up in Skype, Zoom, Teams
+    # https://github.com/umlaeute/v4l2loopback
+    options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
+  '';
+
   networking.hostName = "prentiss";
 
   networking.useDHCP = false;
