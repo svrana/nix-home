4ed3663a3d9a4f056e6ed6d44679b3d5deed8340
Shaw Vrana <shaw@vranix.com>
2022-09-01 16:55:43 -0700

    Update ctlptl

--- packages/ctlptl/default.nix
+++ packages/ctlptl/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "ctlptl";
-  version = "0.7.8";
+  version = "0.8.6";
 
   src = fetchFromGitHub {
     owner = "tilt-dev";
     repo = pname;
     rev = "v${version}";
-    sha256 = "li+WwDmJ5aKmKVg8n/gWmJSBafDa4WLSNitIVBk0ZOY=";
+    sha256 = "sha256-FJtp4g4kIkXFYvYcM9yF3BY6tgHmip11/oIyMSfTwqM=";
   };
 
-  vendorSha256 = "JpCpyjH4XhPPzgUv/yICVR1p7jV7bOIVbKHe28i+Hgc=";
+  vendorSha256 = "sha256-s+Cc7pG/GLK0ZhXX/wK7jMNcDIeu/Am2vCgzrNXKpdw=";
 
   ldFlags = [
     "-s"
