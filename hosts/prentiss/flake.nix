b4d6aeeb8cf32c95d5f63680302503e3c6c364a6
Shaw Vrana <shaw@vranix.com>
2022-09-14 10:48:05 -0700

    Remove old flakes

--- hosts/prentiss/flake.nix
+++
@@ -1,9 +0,0 @@
-{
-  outputs = { self, nixpkgs, ... }@attrs: {
-    nixosConfigurations.prentiss = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = attrs;
-      modules = [ ./configuration.nix ];
-    };
-  };
-}
