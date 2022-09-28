27473ec51b84d12c9cfc5433dfd46405d7250406
Shaw Vrana <shaw@vranix.com>
2022-09-28 14:37:44 -0700

    nvim: add ts playground

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -514,6 +514,32 @@ in
           }
         '';
       }
+      {
+        plugin = playground;
+        type = "lua";
+        config = ''
+          require "nvim-treesitter.configs".setup {
+            playground = {
+              enable = true,
+              disable = {},
+              updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+              persist_queries = false, -- Whether the query persists across vim sessions
+              keybindings = {
+                toggle_query_editor = 'o',
+                toggle_hl_groups = 'i',
+                toggle_injected_languages = 't',
+                toggle_anonymous_nodes = 'a',
+                toggle_language_display = 'I',
+                focus_language = 'f',
+                unfocus_language = 'F',
+                update = 'R',
+                goto_node = '<cr>',
+                show_help = '?',
+              },
+            }
+          }
+        '';
+      }
       popup-nvim
       telescope-fzf-native-nvim
       #{
@@ -647,7 +673,7 @@ in
         '';
       }
       lsp_signature-nvim
-      friendly-snippets
+      #friendly-snippets
       {
         plugin = luasnip;
         type = "lua";
