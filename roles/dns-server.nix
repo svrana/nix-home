77c45381212600a26b9c68cf4f55f4839a64e7d6
Shaw Vrana <shaw@vranix.com>
2022-11-01 17:07:58 -0700

    update adguardhome settings as required by upstream

--- roles/dns-server.nix
+++ roles/dns-server.nix
@@ -9,6 +9,14 @@
   services.adguardhome = {
     enable = true;
     openFirewall = true;
+    settings.dns.bind_host = "0.0.0.0";
+    settings.dns.bootstrap_dns = [
+      "9.9.9.10"
+      "149.112.112.10"
+      "2620:fe::10"
+      "2620:fe::fe:10"
+    ];
+
   };
 
   networking.firewall = {
