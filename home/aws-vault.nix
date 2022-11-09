bc86521b15614aea12e4bce3dc30c323809e91df
Shaw Vrana <shaw@vranix.com>
2022-11-09 11:11:05 -0800

    aws-vault: use standard secret-service
    Be nice to get this to work with gopass

--- home/aws-vault.nix
+++ home/aws-vault.nix
@@ -5,7 +5,7 @@
   ];
 
   programs.bash.sessionVariables = {
-    AWS_VAULT_BACKEND = "file";
+    AWS_VAULT_BACKEND = "secret-service"; # no gopass support, though does support pass
     AWS_VAULT_PASS_PREFIX = "vault";
     AWS_VAULT_PASS_PASSWORD_STORE_DIR = "$XDG_DATA_HOME/password-store";
   };
