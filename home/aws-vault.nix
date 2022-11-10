d6e5662d701191de66aee64a305b598e2d98a24b
Shaw Vrana <shaw@vranix.com>
2022-11-10 12:11:57 -0800

    aws-vault: customize pass cmd to use gopass

--- home/aws-vault.nix
+++ home/aws-vault.nix
@@ -5,7 +5,8 @@
   ];
 
   programs.bash.sessionVariables = {
-    AWS_VAULT_BACKEND = "secret-service"; # no gopass support, though does support pass
+    AWS_VAULT_BACKEND = "pass";
+    AWS_VAULT_PASS_CMD = "gopass";
     AWS_VAULT_PASS_PREFIX = "vault";
     AWS_VAULT_PASS_PASSWORD_STORE_DIR = "$XDG_DATA_HOME/password-store";
   };
