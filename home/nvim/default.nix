0b35d6ab6311561013e278e3182650a32df133b4
Shaw Vrana <shaw@vranix.com>
2023-07-18 12:10:52 -0700

    bye bye flash
    Was interrupting flows some behaviors I don't remember/didn't
    configuring.

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -423,20 +423,20 @@ in
           })
         '';
       }
-      {
-        plugin = flash-nvim;
-        type = "lua";
-        config = ''
-          vim.keymap.set({ "n", "x", "o" }, "s", function() require("flash").jump() end)
-          vim.keymap.set({ "n", "o", "x" }, "S", function() require("flash").treesitter() end)
-          vim.keymap.set("o", "r", function() require("flash").remote() end )
-          vim.keymap.set({ "o", "x" }, "R", function() require("flash").treesitter_search() end)
-
-          vim.keymap.set("c", "<c-s>", function() require("flash").toggle() end)
-
-          require('flash').setup()
-        '';
-      }
+# This is nice but was also triggering during actions where i didn't think it should..or where i configured it.
+#      { plugin = flash-nvim;
+#        type = "lua";
+#        config = ''
+#          vim.keymap.set({ "n", "x", "o" }, "s", function() require("flash").jump() end)
+#          vim.keymap.set({ "n", "o", "x" }, "S", function() require("flash").treesitter() end)
+#          vim.keymap.set("o", "r", function() require("flash").remote() end )
+#          vim.keymap.set({ "o", "x" }, "R", function() require("flash").treesitter_search() end)
+#
+#          vim.keymap.set("c", "<c-s>", function() require("flash").toggle() end)
+#
+#          require('flash').setup()
+#        '';
+#      }
       goimpl-nvim
       #lualine-lsp-progress
       {
