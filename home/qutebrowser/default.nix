cae7ca4b51535d24d75e1e191c7e3891c61a1b69
Shaw Vrana <shaw@vranix.com>
2023-08-31 17:00:06 -0700

    qutebrowser: search shortcuts for rust

--- home/qutebrowser/default.nix
+++ home/qutebrowser/default.nix
@@ -58,14 +58,14 @@ in
     searchEngines = {
       DEFAULT = "https://kagi.com/search?q={}";
       #DEFAULT = "https://duckduckgo.com/?q={}";
-      #DEFAULT = "https://neeva.com/search?q={}";
       d = "https://duckduckgo.com/?q={}";
-      n = "https://neeva.com/search?q={}";
       k = "https://kagi.com/search?q={}";
       g = "http://www.google.com/search?hl=en&q={}";
       gh = "https://github.com/search?q={}";
       yt = "https://www.youtube.com/results?search_query={}";
       ho = "https://hoogle.haskell.org/?hoogle={}";
+      rs = "https://doc.rust-lang.org/std/index.html?search={}";
+      rc = "https://docs.rs/releases/search?query={}";
     };
     keyBindings = {
       command = {
