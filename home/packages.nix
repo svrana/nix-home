9c37da38d996e1b54a8b3ad9c8f4f94f53aa6701
Shaw Vrana <shaw@vranix.com>
2023-09-06 17:16:11 -0700

    Move packages out to packages.nix
    Mostly for git management, to keep changes to default.nix to
    more important ones.

---
+++ home/packages.nix
@@ -0,0 +1,96 @@
+{ pkgs, ... }:
+{
+  home.packages = with pkgs; [
+    autotiling
+    dante
+    dbeaver
+    discord
+    diffstat
+    docker-compose
+    emacs
+    exa
+    networkmanager_dmenu
+    cachix
+    ctlptl
+    google-chrome
+    gh
+    niv # am i using this?
+    element-desktop # matrix client
+    entr
+    evince
+    gimp
+    gnupg
+    kind
+    pkgs.kubernetes-helm
+    gitAndTools.hub
+    grpcurl
+    kubectx
+    ledger-live-desktop
+    i3-ratiosplit # works for sway too
+    imv
+    libsixel # for img2sixel
+    maim # screenshot
+    mpv
+    gnome.nautilus
+    gnome.eog
+    libreoffice
+    nixfmt
+    nixpkgs-review
+    packer
+    pulumi-bin
+    prototool
+    python3
+    readline
+    rnix-lsp
+    shellcheck
+    shfmt
+    spotify-tui
+    slack
+    ssh-agents
+    standardnotes
+    system-san-francisco-font
+    sysz
+    tdesktop # telegram
+    tilt
+    tealdeer
+    tmuxinator
+    tree
+    urlview
+    w3m
+    wmctrl
+    xdg-utils
+    zoom
+    yarn
+    bc
+    vanilla-dmz
+    yq
+    buf
+    #go-migrate
+    nodejs
+    typescript
+    postman
+    protoc-gen-validate
+    #cilium-cli
+    #certbot
+    #istioctl
+    luakit
+
+    obs-studio
+
+    psst
+
+    # sway/wayland specific
+    avizo
+    clipman
+    swaylock
+    swayidle
+    wl-clipboard
+    wf-recorder # i.e., wf-recorder -g "$(slurp)"
+    swaybg
+    waybar
+    wtype
+    slurp
+    grim
+    brightnessctl
+  ];
+}
