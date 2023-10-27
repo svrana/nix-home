512a2d8a2e3f7a36964153d622f1e71f74df9ccb
Shaw Vrana <shaw@vranix.com>
2023-10-27 11:57:49 -0700

    Remove KUBECONFIG
    Not a whole lot uses it, so ~/.kube still has to exist. Might as well
    keep the config in there too.

--- home/kubectl.nix
+++ home/kubectl.nix
@@ -3,9 +3,6 @@
   home.packages = with pkgs; [
     kubectl
   ];
-  programs.bash.sessionVariables = {
-      KUBECONFIG = "$XDG_CONFIG_HOME/kube/config";
-  };
   programs.bash.initExtra = ''
       source ${pkgs.kubectl}/share/bash-completion/completions/kubectl.bash
       complete -F __start_kubectl k
