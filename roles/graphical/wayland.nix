216e1c03a8bac7435bf80ca13bc38aac25ba3b51
Shaw Vrana <shaw@vranix.com>
2024-03-01 06:57:35 -0800

    xdg portal: use first found

--- roles/graphical/wayland.nix
+++ roles/graphical/wayland.nix
@@ -3,6 +3,7 @@
   # for screen sharing
   xdg.portal = {
     enable = true;
+    config.common.default = "*"; # first portal implementation found (lexigraphical),w
     extraPortals = [ pkgs.xdg-desktop-portal-wlr ];
   };
 }
