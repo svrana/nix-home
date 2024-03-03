31cd5dfcb4ef606717ba3879be517e2c246b9e6e
Shaw Vrana <shaw@vranix.com>
2024-03-03 07:40:30 -0800

    sway: don't set default portal
    nixos module will set it and mo better

--- roles/graphical/wayland.nix
+++ roles/graphical/wayland.nix
@@ -6,7 +6,6 @@
   # for screen sharing
   xdg.portal = {
     enable = true;
-    config.common.default = "*"; # first portal implementation found (lexigraphical),w
     extraPortals = [ pkgs.xdg-desktop-portal-wlr ];
   };
 }
