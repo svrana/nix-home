c51e438ff129c0db871efb97a0371efede0db284
Shaw Vrana <shaw@vranix.com>
2024-03-07 07:36:22 -0800

    gruf: remove copypasta cruft

--- home/gruf.nix
+++ home/gruf.nix
@@ -1,15 +1,5 @@
-{ pkgs, lib, ... }:
-
+{ ... }:
 {
-  # programs.fzf = {
-  #   enable = true;
-  #   defaultCommand = "fd --type f";
-  #   defaultOptions = [
-  #     "--bind=ctrl-f:page-down,ctrl-b:page-up,ctrl-d:page-up"
-  #   ];
-  #   fileWidgetCommand = "fd --type f";
-  # };
-
   programs.bash.initExtra = ''
     source "$PROJECTS/gruf/gruf.sh"
   '';
