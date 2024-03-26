7d360f6be206163a499801475a0dab32175fe973
Shaw Vrana <shaw@vranix.com>
2024-03-26 15:32:26 -0700

    cdp: remove some cruft

--- home/cdp.nix
+++ home/cdp.nix
@@ -1,15 +1,5 @@
-{ pkgs, lib, ... }:
-
+{ ... }:
 {
-  # programs.fzf = {
-  #   enable = true;
-  #   defaultCommand = "fd --type f";
-  #   defaultOptions = [
-  #     "--bind=ctrl-f:page-down,ctrl-b:page-up,ctrl-d:page-up"
-  #   ];
-  #   fileWidgetCommand = "fd --type f";
-  # };
-
   programs.bash.initExtra = ''
     export CDP_DIR_SPEC="$PROJECTS:2;$PROJECTS/aws-infra:6"
     export CDP_DEFAULT_VAR="GRUF_PROJECT"
