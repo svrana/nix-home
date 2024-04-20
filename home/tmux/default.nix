75dbfe2047ea899b04ce1cc4084f30c641d7a9e0
Shaw Vrana <shaw@vranix.com>
2024-04-20 16:12:29 -0700

    tmux-sessionizer: style a bit

--- home/tmux/default.nix
+++ home/tmux/default.nix
@@ -1,10 +1,16 @@
-{ pkgs, ... }:
-
+{ pkgs, config, ... }:
+let
+  c = config.settings.theme.withHashTag;
+in
 {
   xdg.configFile."tms/config.toml".text = ''
 [[search_dirs]]
 path = "/home/shaw/Projects"
 depth = 10
+
+[picker_colors]
+highlight_color = "${c.base01}"
+highlight_text_color = "${c.base05}"
   '';
 
   programs.tmux = {
