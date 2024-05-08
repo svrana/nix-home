6f3ceabaf52b0f6a740aaafbfa111504f96905aa
Shaw Vrana <shaw@vranix.com>
2024-05-08 14:14:00 -0700

    nvim: let lualine load config from the theme

--- home/neovim.nix
+++ home/neovim.nix
@@ -1045,7 +1045,6 @@ in
           require('lualine').setup {
            options = {
              icons_enabled = true,
-             theme = 'solarized_dark',
              section_separators = { left = '', right = '' },
              component_separators = { left = '', right = ''},
              disabled_filetypes = {}
