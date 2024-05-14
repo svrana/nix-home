d9e154363feda4c27b7a335ddaec22d4e5c68940
Shaw Vrana <shaw@vranix.com>
2024-05-13 18:03:14 -0700

    Remove some unused scripts

--- home/scripts/vpn-start.sh
+++
@@ -1,3 +0,0 @@
-cd "$DOCUMENTS/synthesis/vpns" && \
-    sudo -E openvpn --config "$DOCUMENTS/synthesis/vpns/client.conf" \
-    --setenv PATH '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' \
