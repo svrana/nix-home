9e9d381a5608054a44426dad9b5d1a05a7202f94
Shaw Vrana <shaw@vranix.com>
2024-07-15 10:10:19 -0700

    direnv: use the silent option
    instead of configuring that myself

--- home/direnv/default.nix
+++ home/direnv/default.nix
@@ -1,13 +1,10 @@
-{ pkgs, lib, ... }:
+{ ... }:
 
 {
   programs.direnv = {
     enable = true;
     stdlib = builtins.readFile ./direnvrc;
     nix-direnv.enable = true;
+    silent = true;
   };
-
-  programs.bash.initExtra = ''
-    export DIRENV_LOG_FORMAT=
-  '';
 }
