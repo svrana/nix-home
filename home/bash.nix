71f6c66a260bce47a7108e44def75f745583f662
Shaw Vrana <shaw@vranix.com>
2024-09-04 12:56:42 -0700

    Split required in bashrc functions.sh into shorter.sh

--- home/bash.nix
+++ home/bash.nix
@@ -8,20 +8,13 @@
     shellOptions = [ "histappend" "checkwinsize" "autocd" "cdspell" ];
     # only run for interactive sessions
     initExtra = ''
-      source "$RCS/bash/functions.sh"
+      source "$RCS/bash/shorter.sh"
+      #source "$RCS/bash/functions.sh"
       #source "$PERSONAL/bommie.io/env.sh"
       #source "$PERSONAL/work/env.sh"
 
       set -o vi
       stty -ixon
-
-      is() {
-        if [ -z "$1" ]; then
-            return
-        fi
-
-        ps -ef | head -n1 ; ps -ef | grep -v grep | grep "$@" -i --color=auto;
-      }
     '';
     bashrcExtra = ""; # run prior to the interactive session check
     profileExtra = "";
---
+++ home/config/bash/shorter.sh
@@ -0,0 +1,64 @@
+alias path='echo -e ${PATH//:/\\n}'
+alias svi='sudo vi'
+
+is() {
+    if [ -z "$1" ]; then
+        return
+    fi
+
+    ps -ef | head -n1 ; ps -ef | grep -v grep | grep "$@" -i --color=auto;
+}
+
+md() {
+    [ -z "$1" ] && return
+    mkdir "$1" && cd "$1"
+}
+
+s() {
+    if [[ $# == 0 ]]; then
+        sudo $(history -p '!!')
+    else
+        sudo "$@"
+    fi
+}
+
+linkme() {
+    [ -z "$1" ] && return
+    readlink -f $(which "$1")
+}
+
+# run command until it succeeds
+loopit() {
+    if [ -z "$1" ]; then
+        echo 'Must provide command to loop'
+        return 1
+    fi
+
+    while true; do
+        $(echo "$@") && break
+        sleep .05 # allow ctrl-c out
+    done
+}
+
+# run command until it fails
+loopitfail() {
+    if [ -z "$1" ]; then
+        echo 'Must provide command to loop'
+        return 1
+    fi
+
+    while true; do
+        $(echo "$@") || break
+        sleep .05 # allow ctrl-c out
+    done
+}
+
+flakify() {
+  if [ ! -e flake.nix ]; then
+    nix flake new -t github:nix-community/nix-direnv .
+  elif [ ! -e .envrc ]; then
+    echo "use flake" > .envrc
+    direnv allow
+  fi
+  ${EDITOR:-vim} flake.nix
+}
