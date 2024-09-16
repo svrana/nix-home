e36994c0da552509a8291cf0b50a9f5b906e26be
Shaw Vrana <shaw@vranix.com>
2024-09-15 17:17:12 -0700

    nvim: switch to rustaceanvim

--- home/neovim.nix
+++ home/neovim.nix
@@ -287,21 +287,8 @@ in
         '';
       }
       {
-        plugin = rust-tools-nvim;
+        plugin = rustaceanvim;
         type = "lua";
-        config = ''
-          local rt = require("rust-tools")
-          rt.setup({
-            server = {
-              on_attach = function(_, bufnr)
-              -- Hover actions
-              vim.keymap.set("n", "<C-Space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-              -- Code action groups
-              vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-              end,
-            },
-          })
-        '';
       }
       {
         plugin = harpoon;
