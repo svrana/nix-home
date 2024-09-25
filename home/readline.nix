1daf146f5e32d19389ca0e2ce624dc2fcaee3f56
Shaw Vrana <shaw@vranix.com>
2024-09-25 14:45:01 -0700

    readline: format changes
    still no longer works

--- home/readline.nix
+++ home/readline.nix
@@ -3,13 +3,10 @@
   programs.readline = {
     enable = true;
     bindings = {
-      "\\C-l" = "clear-screen";
-      "\\C-o"  = "source ~/.bashrc && [ -f .envrc ] && direnv reload > /dev/null 2>&1\n";
+      "C-l" = "clear-screen";
+      "C-o"  = "source ~/.bashrc && [ -f .envrc ] && direnv reload > /dev/null 2>&1\n";
     };
     extraConfig = ''
-      set show-mode-in-prompt on
-      set vi-ins-mode-string "\1\e[2 q\2"
-      set vi-cmd-mode-string "\1\e[6 q\2"
       set editing-mode vi
     '';
   };
