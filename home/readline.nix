9fdd782b0c8bb2f66680eacdacf39a10a92d7863
Shaw Vrana <shaw@vranix.com>
2024-10-09 13:33:01 -0700

    readline: map ctrl-l clear-screen for both vi modes

--- home/readline.nix
+++ home/readline.nix
@@ -2,12 +2,16 @@
 {
   programs.readline = {
     enable = true;
-    bindings = {
-      "C-l" = "clear-screen";
-      "C-o"  = "source ~/.bashrc && [ -f .envrc ] && direnv reload > /dev/null 2>&1\n";
-    };
+    # example how to set keybindings in inserts and command mode. Note that I'm ctrl-l to move in tmux terminal
+    # so doesn't work there.
     extraConfig = ''
       set editing-mode vi
+      $if mode=vi
+        set keymap vi-command
+        Control-l: clear-screen
+        set keymap vi-insert
+        Control-l: clear-screen
+      $endif
     '';
   };
 }
