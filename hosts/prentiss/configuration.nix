843a945da12daf26e5a7236b8eefe0eed7d54d67
Shaw Vrana <shaw@vranix.com>
2025-01-03 08:56:18 -0800

    Remove some kernel video config
    Think I was trying to get screen sharing working with obs, but not
    using.

--- hosts/prentiss/configuration.nix
+++ hosts/prentiss/configuration.nix
@@ -12,21 +12,7 @@
     ../../roles/android-dev.nix
   ];
 
-  boot.extraModulePackages = with config.boot.kernelPackages;
-    [ v4l2loopback.out ];
-  boot.kernelModules = [
-    "v4l2loopback"
-    "snd-aloop"
-  ];
-  boot.extraModprobeConfig = ''
-    # exclusive_caps: Skype, Zoom, Teams etc. will only show device when actually streaming
-    # card_label: Name of virtual camera, how it'll show up in Skype, Zoom, Teams
-    # https://github.com/umlaeute/v4l2loopback
-    options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
-  '';
-
   networking.hostName = "prentiss";
-
   networking.useDHCP = false;
   # using network manager which uses it's own dhcp client so do not need to startup dhcpcd here. Doing so
   # results in two ip addresses for the same interfaces, which isn't horrible but can make it harder to configure
