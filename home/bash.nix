9eb46f78e86cdc4b2bc5bad0fa5d0608bafca54a
Shaw Vrana <shaw@vranix.com>
2025-03-12 18:39:34 -0700

    bash: source some files at startup iff they exist

--- home/bash.nix
+++ home/bash.nix
@@ -11,7 +11,12 @@
       source "$RCS/bash/shorter.sh"
       #source "$RCS/bash/functions.sh"
       #source "$PERSONAL/bommie.io/env.sh"
-      #source "$PERSONAL/work/env.sh"
+      if [ -f $PERSONAL/work/env.sh ]; then
+        source $PERSONAL/work/env.sh
+      fi
+      if [ -f $PERSONAL/env.sh ]; then
+        source $PERSONAL/env.sh
+      fi
 
       set -o vi
       stty -ixon
