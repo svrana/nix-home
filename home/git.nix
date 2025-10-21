ce81243cc2066b44dc8055837d02bb52a04e55ca
Shaw Vrana <shaw@vranix.com>
2025-10-21 12:43:59 -0700

    update

--- home/git.nix
+++ home/git.nix
@@ -5,49 +5,48 @@ in
 {
   programs.git = {
     enable = true;
-    userName = "Shaw Vrana";
-    userEmail = "shaw@vranix.com";
     signing = {
       key = "220B8643296AB53D";
       signByDefault = false;
     };
-    aliases = {
-      rename = "commit --reset-author --amend";
-      co = "checkout";
-      ci = "commit";
-      ciaa = "commit -a --amend";
-      cia = "commit --amend";
-      mt = "mergetool";
-      st = "status";
-      ps = "!git push origin $(git rev-parse --abbrev-ref HEAD)";
-      pl = "!git pull origin $(git rev-parse --abbrev-ref HEAD)";
-      cat = "cat-file";
-      b = "branch --color -v";
-      changes = "diff --name-status -r";
-      diffstat = "diff --stat -r";
-      whois = ''
-        !sh -c 'git log -i -1 --pretty="format:%an <%ae>
-        " --author="$1"' -'';
-      whatis = "show -s --pretty='tformat:%h (%s, %ad)' --date=short";
-      edit-unmerged =
-        "!f() { git ls-files --unmerged | cut -f2 | sort -u ; }; vim `f`";
-      add-unmerged =
-        "!f() { git ls-files --unmerged | cut -f2 | sort -u ; }; git add `f`";
-      lg = "log --pretty=format:'%C(cyan)%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
-      latest = "diff HEAD~1 HEAD";
-      home = ''config user.email "shaw@vranix.com"'';
-      work = ''config user.email "shaw@vrana.com"'';
-      hg = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
-      fr = "!f() { git fetch origin main && git rebase origin/main; }; f";
-      fro = "!f() { git fetch origin master && git rebase origin/master; }; f";
-      new = ''!f() { git checkout -b "$@" --track origin/main; }; f'';
-      patch = "!git --no-pager diff --no-color";
-      cb = "!sh $HOME/.local/bin/cb";
-      wt = "worktree";
-      cln ="clean -fd";
-    };
-    lfs = { enable = true; };
-    extraConfig = {
+      settings = {
+        user.name = "Shaw Vrana";
+        user.email = "shaw@vranix.com";
+        alias = {
+          rename = "commit --reset-author --amend";
+          co = "checkout";
+          ci = "commit";
+          ciaa = "commit -a --amend";
+          cia = "commit --amend";
+          mt = "mergetool";
+          st = "status";
+          ps = "!git push origin $(git rev-parse --abbrev-ref HEAD)";
+          pl = "!git pull origin $(git rev-parse --abbrev-ref HEAD)";
+          cat = "cat-file";
+          b = "branch --color -v";
+          changes = "diff --name-status -r";
+          diffstat = "diff --stat -r";
+          whois = ''
+          !sh -c 'git log -i -1 --pretty="format:%an <%ae>
+          " --author="$1"' -'';
+          whatis = "show -s --pretty='tformat:%h (%s, %ad)' --date=short";
+          edit-unmerged =
+            "!f() { git ls-files --unmerged | cut -f2 | sort -u ; }; vim `f`";
+          add-unmerged =
+            "!f() { git ls-files --unmerged | cut -f2 | sort -u ; }; git add `f`";
+          lg = "log --pretty=format:'%C(cyan)%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
+          latest = "diff HEAD~1 HEAD";
+          home = ''config user.email "shaw@vranix.com"'';
+          work = ''config user.email "shaw@vrana.com"'';
+          hg = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+          fr = "!f() { git fetch origin main && git rebase origin/main; }; f";
+          fro = "!f() { git fetch origin master && git rebase origin/master; }; f";
+          new = ''!f() { git checkout -b "$@" --track origin/main; }; f'';
+          patch = "!git --no-pager diff --no-color";
+          cb = "!sh $HOME/.local/bin/cb";
+          wt = "worktree";
+          cln ="clean -fd";
+        };
       init = {
         defaultBranch = "main";
       };
@@ -95,6 +94,7 @@ in
       #   "diffFilter" = "delta --color-only";
       # };
     };
+    lfs = { enable = true; };
     ignores = [
       ".gruf.*"
       "cscope.out"
