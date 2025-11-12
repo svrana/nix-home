d2a8441e80b87fd84f20f057132277f852b5d8fd
Shaw Vrana <shaw@vranix.com>
2025-11-12 13:14:50 -0800

    nvim: fix indent

--- home/neovim.nix
+++ home/neovim.nix
@@ -641,9 +641,9 @@ in
           vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump(-1) end, {silent = true})
 
           vim.keymap.set({"i", "s"}, "<C-E>", function()
-	        if ls.choice_active() then
-		      ls.change_choice(1)
-	        end
+            if ls.choice_active() then
+              ls.change_choice(1)
+            end
           end, {silent = true})
         '';
       }
