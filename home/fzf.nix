f962c476db3e6d1f074e3538cc163f45caeac3e4
Shaw Vrana <shaw@vranix.com>
2025-11-17 19:22:46 -0800

    fzf: remove commented out code

--- home/fzf.nix
+++ home/fzf.nix
@@ -20,11 +20,5 @@ in
   programs.bash.initExtra = ''
     # open a file under the current directory
     bind -x '"\C-p": f() { local file=$(fzf -m --height 80%) && [[ -n $file ]] && $EDITOR $file ; }; f'
-
-    # open a dotfile
-    #bind -x '"\C-n": f() { pushd $DOTFILES > /dev/null ; local file=$(fzf -m --height 80% --preview "bat --style=numbers --color=always --line-range :100 {}") && [[ -n $file ]] && $EDITOR $file ; popd > /dev/null ; }; f'
-
-    # open a file under the current (git) project root
-    #bind -x '"\C-f": f() { pushd $(git_root) > /dev/null ; local file=$(fzf -m --height 80%) && [[ -n $file ]] && $EDITOR $file ; popd > /dev/null ; }; f'
   '';
 }
