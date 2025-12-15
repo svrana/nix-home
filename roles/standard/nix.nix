8ff46adfe2ec455f3c436fb5aeac0a3dc223820f
Shaw Vrana <shaw@vranix.com>
2025-12-15 13:32:20 -0800

    nix: remove some now unused substituters

--- roles/standard/nix.nix
+++ roles/standard/nix.nix
@@ -8,15 +8,11 @@
       substituters = [
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
-        "https://nrdxp.cachix.org"
-        "https://digitallyinduced.cachix.org"
 
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "nrdxp.cachix.org-1:Fc5PSqY2Jm1TrWfm88l6cvGWwz3s93c6IOifQWnhNW4="
-        "digitallyinduced.cachix.org-1:y+wQvrnxQ+PdEsCt91rmvv39qRCYzEgGQaldK26hCKE="
       ];
       auto-optimise-store = true;
     };
