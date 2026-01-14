9c015144024172f37f8f5619a58713405dcb2c9d
Shaw Vrana <shaw@vranix.com>
2026-01-13 19:33:59 -0800

    tailscale: bring up to date

--- roles/tailscale.nix
+++ roles/tailscale.nix
@@ -1,21 +1,17 @@
 { config, ... }:
 {
-  services.tailscale.enable = true;
+  services.tailscale = {
+    enable = true;
+    openFirewall = true;
+    useRoutingFeatures = "client";
+  };
 
   # Attempt to wait until online service is finished, otherwise wait online will fail.
   # See: https://github.com/NixOS/nixpkgs/issues/180175
   systemd.services.tailscaled.after = ["NetworkManager-wait-online.service"];
 
   networking.firewall = {
-    # Strict reverse path filtering breaks Tailscale exit node use and some subnet routing setup
-    checkReversePath = "loose";
-    # enable the firewall
-    enable = true;
     # always allow traffic from your Tailscale network
     trustedInterfaces = [ "tailscale0" ];
-
-    allowedUDPPorts = [
-      config.services.tailscale.port
-    ];
   };
 }
