77078f5d4604be94481e08c1aaeb4f44ec4e1dc3
Shaw Vrana <shaw@vranix.com>
2026-01-14 11:36:44 -0800

    add navidrome to bocana

--- roles/media.nix
+++ roles/media.nix
@@ -36,4 +36,13 @@
       type = "vaapi";
     };
   };
+
+  services.navidrome = { # port 4533
+    enable = true;
+    openFirewall = true;
+    settings = {
+      Address = "0.0.0.0";
+      MusicFolder = "/var/lib/syncthing/Cloud/Music";
+    };
+  };
 }
