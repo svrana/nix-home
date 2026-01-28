3d6b2e7368763ad954f4d30497af4445c94bbc6b
Shaw Vrana <shaw@vranix.com>
2026-01-28 12:14:02 -0800

    migrate from flake-utils to flake-parts

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -22,7 +22,6 @@
     };
     spotify-cleanup = {
       url = "github:svrana/spotify-cleanup";
-      #url = "git+file:///home/shaw/Projects/spotify-cleanup";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     neorg-overlay = {
@@ -33,113 +32,103 @@
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixflix = {
-      url = "github:kiriwalawren/nixflix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    , deploy-rs
-    , home-manager
-    , neorg-overlay
-    , sops-nix
-    , nixflix
-    , ... } @inputs:
-    let
-      inherit (nixpkgs.lib) nixosSystem;
-      nixpkgsConfig = {
-        config = {
-          allowUnfree = true;
-          allowUnfreePredicate = (pkgs: true);
+  outputs = inputs@{ self, flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+
+      perSystem = { system, pkgs, ... }: {
+        devShells.default = pkgs.mkShell {
+          name = "dots";
+          buildInputs = [
+            inputs.deploy-rs.packages.${system}.deploy-rs
+            (import inputs.home-manager { inherit pkgs; }).home-manager
+          ];
         };
-        overlays = builtins.attrValues self.overlays;
       };
 
-      mkSystem = extraModules: nixosSystem {
-          system = "x86_64-linux";
-          specialArgs = { inherit inputs; };
-          modules = [
-            { nixpkgs.overlays = nixpkgsConfig.overlays; }
-            sops-nix.nixosModules.sops
-            nixflix.nixosModules.default
-          ] ++ extraModules;
+      flake = let
+        inherit (nixpkgs.lib) nixosSystem;
+        lib = nixpkgs.lib;
+
+        nixpkgsConfig = {
+          config = {
+            allowUnfree = true;
+            allowUnfreePredicate = _: true;
+          };
+          overlays = builtins.attrValues self.overlays;
         };
 
-      mkHome = extraModules: home-manager.lib.homeManagerConfiguration {
-          pkgs = import nixpkgs {
+        mkSystem = extraModules:
+          nixosSystem {
             system = "x86_64-linux";
-            inherit (nixpkgsConfig) config overlays;
+            specialArgs = { inherit inputs; };
+            modules = [
+              { nixpkgs.overlays = nixpkgsConfig.overlays; }
+              inputs.sops-nix.nixosModules.sops
+            ] ++ extraModules;
           };
-          modules = [
-            modules/my.nix
-            inputs.wayland-pipewire-idle-inhibit.homeModules.default
-            inputs.spotify-cleanup.homeModules.default
-            inputs.sops-nix.homeManagerModules.sops
-          ] ++ extraModules;
-          extraSpecialArgs = { inherit inputs; };
-        };
-    in
-    (
-      flake-utils.lib.eachDefaultSystem
-        (system:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-          in
-          {
-            devShells.default= pkgs.mkShell {
-              name = "dots";
-              buildInputs = [
-                deploy-rs.packages.${system}.deploy-rs
-                (import home-manager { inherit pkgs; }).home-manager
-              ];
+
+        mkHome = extraModules:
+          inputs.home-manager.lib.homeManagerConfiguration {
+            pkgs = import nixpkgs {
+              system = "x86_64-linux";
+              inherit (nixpkgsConfig) config overlays;
             };
-          })
-      //
-      {
+            modules = [
+              ./modules/my.nix
+              inputs.wayland-pipewire-idle-inhibit.homeModules.default
+              inputs.spotify-cleanup.homeModules.default
+              inputs.sops-nix.homeManagerModules.sops
+            ] ++ extraModules;
+            extraSpecialArgs = { inherit inputs; };
+          };
+      in {
         nixosConfigurations = {
           prentiss = mkSystem [ ./hosts/prentiss/configuration.nix ];
-          bocana = mkSystem [ ./hosts/bocana/configuration.nix ];
-          park = mkSystem [ ./hosts/park/configuration.nix ];
+          bocana   = mkSystem [ ./hosts/bocana/configuration.nix ];
+          park     = mkSystem [ ./hosts/park/configuration.nix ];
         };
 
         homeConfigurations = {
           prentiss = mkHome [ ./hosts/prentiss ];
-          park = mkHome [ ./hosts/park ];
+          park     = mkHome [ ./hosts/park ];
         };
 
-        overlays = with nixpkgs.lib;
+        overlays = with lib;
           let
-            overlayFiles' = filter (hasSuffix ".nix") (attrNames (builtins.readDir ./overlays));
-            overlayFiles = listToAttrs (map
-              (name: {
-                name = removeSuffix ".nix" name;
-                value = import (./overlays + "/${name}");
-              })
-              overlayFiles');
+            overlayFiles' =
+              filter (hasSuffix ".nix") (attrNames (builtins.readDir ./overlays));
+
+            overlayFiles =
+              listToAttrs (map
+                (name: {
+                  name = removeSuffix ".nix" name;
+                  value = import (./overlays + "/${name}");
+                })
+                overlayFiles');
           in
           overlayFiles // {
-              neorg-overlay = neorg-overlay.overlays.default;
+            neorg-overlay = inputs.neorg-overlay.overlays.default;
           };
 
-        # deploy '.#bocana'
         deploy.nodes.bocana = {
           hostname = "bocana";
           autoRollback = false;
           sshUser = "root";
           magicRollback = false;
           fastConnection = true;
+
           profiles.system = {
             user = "root";
-            path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.bocana;
+            path =
+              inputs.deploy-rs.lib.x86_64-linux.activate.nixos
+                self.nixosConfigurations.bocana;
           };
         };
 
-        # This is highly advised, and will prevent many possible mistakes
-        checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
-      }
-    );
+        checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) inputs.deploy-rs.lib;
+      };
+    };
 }
