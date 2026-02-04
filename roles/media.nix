10165706b6e310d554ea52ba01b4f32d9dfd0b9f
Shaw Vrana <shaw@vranix.com>
2026-02-04 13:00:12 -0800

    Remove arr stack from media.nix

--- roles/media.nix
+++ roles/media.nix
@@ -1,47 +1,5 @@
 { pkgs, config, ... }:
 {
-  services.sonarr = {
-    enable = true;
-    openFirewall = true;
-  };
-
-  services.radarr = {
-    enable = true;
-    openFirewall = true; # port 7878
-  };
-
-  services.sabnzbd = {
-    enable = true;
-    allowConfigWrite = true;
-    openFirewall = true;
-    settings = {
-      misc = {
-        host = "0.0.0.0";
-        port = 8080;
-      };
-    };
-  };
-
-  services.prowlarr = {
-    enable = true;
-    openFirewall = true; # port 9696
-  };
-
-  services.jellyfin = {
-    enable = true;
-    openFirewall = true; # port 8096, 8920
-    hardwareAcceleration = {
-      enable = true;
-      device = "/dev/dri/renderD128";
-      type = "vaapi";
-    };
-  };
-
-  services.jellyseerr = {
-    enable = true;
-    openFirewall = true; # port 5055
-  };
-
   services.navidrome = { # port 4533
     enable = true;
     openFirewall = true;
